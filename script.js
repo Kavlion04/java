f00b49bcd6f36dbe4761a361802bc17306e3e325
aqqullo <xaqqulloabdusamotov@gmail.com>
2024-11-28 23:50:20 +0500

    5 masala

---
+++ script.js
@@ -0,0 +1,57 @@
+
+// 1chi masala 
+/*
+number = Number(prompt("Uch xonali sonni kiriting: "))
+let hundreds = Math.floor(number / 100)
+let tens = Math.floor((number / 10) % 10)
+let units = number % 10  
+let new_number = units * 100 + hundreds * 10 + tens
+prompt("Natija:", new_number) 
+*/
+
+
+//2chi masala
+
+/*
+number = Number(prompt("999 dan katta bo'lgan sonni kiriting: "))
+thousands_digit =(number / 1000) % 10
+prompt("Mingliklar xonasidagi raqam:", thousands_digit)  
+*/
+
+
+//3chi masala
+/*
+number = Number(prompt("Uch xonali sonni kiriting: "))
+let hundreds = Math.floor(number / 100)
+let tens = Math.floor((number / 10) % 10)
+let units = number % 10  
+let new_number = units + hundreds  + tens
+prompt("yigindi:", new_number)
+ */
+
+
+
+// 4chi masala
+/*
+number = Number(prompt("Uch xonali sonni kiriting: "))
+hundreds = number / 100  
+tens = (number / 10) % 10
+units = number % 10 
+reversed_number = units * 100 + tens * 10 + hundreds
+prompt("Teskari tartibda hosil bo'lgan son:", reversed_number)
+console.log(reversed_number)
+
+*/
+
+
+
+//5chi masala
+/*
+let N = parseInt(prompt("Kun boshidan o'tgan soniyalarni kiriting:"));
+let hours = Math.floor(N / 3600);
+let minutes = Math.floor((N % 3600) / 60);
+let seconds = N % 60;
+console.log(`Kun boshidan ${hours} soat ${minutes} minut va ${seconds} sekund o'tgan.`);
+alert(`Kun boshidan ${hours} soat ${minutes} minut va ${seconds} sekund o'tgan.`);
+*/
+
